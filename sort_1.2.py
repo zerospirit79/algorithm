80b18aaa88401d02e75dbf9a1d0405752f7f0ec7
zerospirit79 <pavel.s.shilov@gmail.com>
2020-10-06 14:22:25 +0300

    updated

---
+++ sort_1.2.py
@@ -0,0 +1,23 @@
+"""
+алгоритм невозвращающего порядка глава 1
+"""
+
+def sort_arr(array):
+    length = len(array)
+    for j in range(1, length):
+        key = array[j]
+        i = j - 1
+        while i >= 0 and array[i] < key:
+            array[i + 1] = array[i]
+            i = i - 1
+        array[i + 1] = key
+
+
+arr = []
+length = int(input("Введите длину массива:"))
+
+for i in range(0, length):
+    element = int(input("arr[" + str(i + 1) + "] = "))
+    arr.append(element)
+sort_arr(arr)
+print(arr)
